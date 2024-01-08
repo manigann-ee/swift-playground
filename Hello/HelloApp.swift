16187f374e26b771c90f3c1547dc679361105bef
Manigandan N <manigandan.n@everest.engineering>
2024-01-08 16:04:39 +0530

    Initial commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Manigandan N on 08/01/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
